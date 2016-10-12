5e3626d697008802e3b299379a8d43474ac60b7e
mrpoore <rpoore@neisd.net>
2016-10-12 11:54:47 -0500

    initial Commit

---
+++ GiTestingkali linux/src/ThisIsATest.java
@@ -0,0 +1,9 @@
+
+public class ThisIsATest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test of the GitHub system");;
+	}
+
+}
